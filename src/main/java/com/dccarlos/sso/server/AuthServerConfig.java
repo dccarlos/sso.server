997932ae4475c01cb7451c153b44d9632b854e27
ccordero <ccordero@homeaway.com>
2019-02-19 19:36:52 -0600

    Add sub claims

--- src/main/java/com/dccarlos/sso/server/AuthServerConfig.java
+++ src/main/java/com/dccarlos/sso/server/AuthServerConfig.java
@@ -1,13 +1,23 @@
 package com.dccarlos.sso.server;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -26,6 +36,11 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(
+                tokenEnhancer(),
+                accessTokenConverter()));
+
         endpoints
                 .pathMapping("/oauth/authorize", "/v1/authorize")
                 .pathMapping("/oauth/token", "/v1/token")
@@ -33,7 +48,9 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .pathMapping("/oauth/logout", "/v1/logout")
                 .pathMapping("/oauth/check_token", "/v1/check_token")
                 .pathMapping("/oauth/confirm_access", "/v1/confirm_access")
-                .pathMapping("/oauth/error", "/v1/error");
+                .pathMapping("/oauth/error", "/v1/error")
+                .tokenStore(tokenStore())
+                .tokenEnhancer(tokenEnhancerChain);
     }
 
     @Override
@@ -45,5 +62,31 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .scopes("openid", "profile", "email")
                 .autoApprove(true)
                 .redirectUris("http://localhost:8080", "http://localhost:8080/api/auth/callback", "http://localhost:8082/ui/login", "http://localhost:8083/ui2/login", "http://localhost:8082/login");
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("123");
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+
+    @Bean
+    public TokenEnhancer tokenEnhancer() {
+        return new CustomTokenEnhancer();
     }
 }
